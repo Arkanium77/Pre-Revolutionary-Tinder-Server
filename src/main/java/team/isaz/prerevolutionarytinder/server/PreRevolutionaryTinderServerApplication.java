f997a798da28c2aa8deae97aec217780239a79c7
Arkanium77 <arkanium77@gmail.com>
2020-04-07 16:14:15 +0300

    Initial commit

---
+++ src/main/java/team/isaz/prerevolutionarytinder/server/PreRevolutionaryTinderServerApplication.java
@@ -0,0 +1,13 @@
+package team.isaz.prerevolutionarytinder.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreRevolutionaryTinderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreRevolutionaryTinderServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/isaz/prerevolutionarytinder/server/PreRevolutionaryTinderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.isaz.prerevolutionarytinder.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreRevolutionaryTinderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
