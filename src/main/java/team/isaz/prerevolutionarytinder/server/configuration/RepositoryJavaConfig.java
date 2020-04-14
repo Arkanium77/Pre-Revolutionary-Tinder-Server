512327497eb6485fbbb5ec030c0a2f8fc826153b
Arkanium77 <arkanium77@gmail.com>
2020-04-14 19:43:07 +0300

    Delete Role entity

--- src/main/java/team/isaz/prerevolutionarytinder/server/configuration/RepositoryJavaConfig.java
+++ src/main/java/team/isaz/prerevolutionarytinder/server/configuration/RepositoryJavaConfig.java
@@ -3,22 +3,18 @@ package team.isaz.prerevolutionarytinder.server.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import team.isaz.prerevolutionarytinder.server.domain.repository.LikeRepository;
-import team.isaz.prerevolutionarytinder.server.domain.repository.RoleRepository;
 import team.isaz.prerevolutionarytinder.server.domain.repository.UserRepository;
 
 @Configuration
 public class RepositoryJavaConfig {
     private final UserRepository userRepository;
     private final LikeRepository likeRepository;
-    private final RoleRepository roleRepository;
 
     @Autowired
     public RepositoryJavaConfig(UserRepository userRepository,
-                                LikeRepository likeRepository,
-                                RoleRepository roleRepository) {
+                                LikeRepository likeRepository) {
         this.userRepository = userRepository;
         this.likeRepository = likeRepository;
-        this.roleRepository = roleRepository;
     }
 
     public UserRepository userRepository() {
@@ -27,10 +23,6 @@ public class RepositoryJavaConfig {
 
     public LikeRepository likeRepository() {
         return this.likeRepository;
-    }
-
-    public RoleRepository roleRepository() {
-        return this.roleRepository;
     }
 }
 
--- src/main/java/team/isaz/prerevolutionarytinder/server/domain/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package team.isaz.prerevolutionarytinder.server.domain.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import team.isaz.prerevolutionarytinder.server.domain.entity.Role;
-
-@Repository
-public interface RoleRepository extends CrudRepository<Role, Integer> {
-}
