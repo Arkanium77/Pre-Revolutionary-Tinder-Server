f793a5468337dec85c5475eda4085dfcf646c2bc
Arkanium77 <arkanium77@gmail.com>
2020-04-15 15:25:40 +0300

    add only-status constructor for Response

--- src/main/java/team/isaz/prerevolutionarytinder/server/domain/Response.java
+++ src/main/java/team/isaz/prerevolutionarytinder/server/domain/Response.java
@@ -3,6 +3,12 @@ package team.isaz.prerevolutionarytinder.server.domain;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+/**
+ * <b>Response</b>
+ * Сущность, используемая для передачи данных между сервисами.
+ * Содержит статус операции и, опционально, дополнительные данные.
+ * Это может быть строка-пояснение, данные необходимые для работы программы или какой-то мета-контейнер.
+ */
 @Getter
 @EqualsAndHashCode
 public class Response {
@@ -11,6 +17,11 @@ public class Response {
 
     public Response() {
         status = false;
+    }
+
+    public Response(boolean status) {
+        this.status = status;
+        this.attach = null;
     }
 
     public Response(boolean status, Object attach) {
